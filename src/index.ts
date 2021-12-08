b42a1055b126ebb08ce252a102c43a4c6253ff0d
Markim Shaw <ms79723@gmail.com>
2021-12-08 09:25:20 -0600

    Add goodmorning clip

--- src/index.ts
+++ src/index.ts
@@ -39,6 +39,7 @@ app.get(
 const questionHash = [
   { key: '0', value: '81309d24-4626-4288-95a3-f0b4892b46fc' },
   { key: '123', value: '3e979c20-175d-419f-a4e5-cf0017659926' },
+  { key: '1', value: 'e5da6d04-2e42-44d5-a3b0-914a4cce2dec' },
 ];
 
 app.get('/test-quiz', async (request: Request, response: Response) => {
